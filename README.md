b99cbdf2c4e384c7789deb9e643662e3e636bd45
vinnyfiore <vinnydakid@gmail.com>
2018-01-02 16:27:37 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Group6Project
---
+++ Results/For Word Clouds/wordCloud_encrption.R
@@ -0,0 +1,24 @@
+# Install
+install.packages("tm")  # for text mining
+install.packages("SnowballC") # for text stemming
+install.packages("wordcloud") # word-cloud generator 
+install.packages("RColorBrewer") # color palettes
+# Load
+library("tm")
+library("SnowballC")
+library("wordcloud")
+library("RColorBrewer")
+filePath <- "file:///C:/Users/HP/Desktop/encryption.txt"
+text <- readLines(filePath)
+# Load the data as a corpus
+docs <- Corpus(VectorSource(text))
+inspect(docs)
+dtm <- TermDocumentMatrix(docs)
+m <- as.matrix(dtm)
+v <- sort(rowSums(m),decreasing=TRUE)
+d <- data.frame(word = names(v),freq=v)
+head(d, 10)
+set.seed(1234)
+wordcloud(words = d$word, freq = d$freq, min.freq = 1,
+          max.words=200, random.order=FALSE, rot.per=0.35, 
+          colors=brewer.pal(8, "Dark2"))
